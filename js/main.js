e69da8c127ded49c1b5ca98e6f18590bf13c6ece
saraen7 <sara.e.norlin@outlook.com>
2017-10-11 21:03:47 -0400

    Background, Nav, and H1
    It's starting to look okay on initial load. Also added a .txt file with next steps

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-  //all js goes here
+
+  //h1 loads line by line on page load
+  var str = "Sounds of East Boston";
+  var spans = '<span>' + str.split(/\s+/).join(' </span><br><span>') + '</span>';
+  $(spans).hide().appendTo('h1').each(function(i) {
+    $(this).delay(125 * i).fadeIn();
+  }); //end h1 loads line by line on page load
+
+  //navigation menu revealed on click
+  $("nav > div").on("click",function(){
+    $("ul").toggleClass("hiddenNav");
+    if(!$("nav > div").hasClass("hiddenNav")){
+      $("li").hide().appendTo("ul").each(function(i){
+        $(this).delay(125 * i).fadeIn();
+      });
+    }
+  }); //end navigation menu revealed on click
+
+  //navigation items appear one at a time
+
+
 });
