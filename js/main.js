ac32101c8e1c4a4c4f650e1e7737c590301e36f0
saraen7 <sara.e.norlin@hotmail.com>
2017-10-22 19:45:06 -0400

    Now the line has mouse over spots
    Yay!

--- js/main.js
+++ js/main.js
@@ -28,18 +28,15 @@ $(document).ready(function(){
   });
 
 //d3 stuff starts here
- //initial d3 test
-d3.select(".d3Test")
-  .append("p")
-  .text("Load text with d3.js! today");
 
   // Set the dimensions of the canvas / graph
   var margin = {top: 30, right: 20, bottom: 30, left: 50},
-      width = 600 - margin.left - margin.right,
-      height = 270 - margin.top - margin.bottom;
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
   // Parse the date / time 2017-06-06 15:21:03
-  var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
+  var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse,
+      bisectDate = d3.bisector(function(d) { return d.date; }).left;
 
   // Set the ranges
   var x = d3.time.scale().range([0, width]);
@@ -47,7 +44,7 @@ d3.select(".d3Test")
 
   // Define the axes
   var xAxis = d3.svg.axis().scale(x)
-      .orient("bottom").ticks(5);
+      .orient("bottom").ticks(10);
 
   var yAxis = d3.svg.axis().scale(y)
       .orient("left").ticks(5);
@@ -77,9 +74,13 @@ d3.select(".d3Test")
 
       // Scale the range of the data
       x.domain(d3.extent(data, function(d) { return d.date; }));
-      y.domain([0, d3.max(data, function(d) { return d.close; })]);
+      //Below is the original Y Axis code that runs from 0 to 100
+      //y.domain([0, d3.max(data, function(d) { return d.close; })]);
+      //This spans the Y axis from the min and max of the data
+      y.domain(d3.extent(data, function(d) { return d.close; }));
 
       // Add the valueline path.
+
       svg.append("path")
           .attr("class", "line")
           .attr("d", valueline(data));
@@ -94,6 +95,35 @@ d3.select(".d3Test")
       svg.append("g")
           .attr("class", "y axis")
           .call(yAxis);
+
+      var focus = svg.append("g")
+         .attr("class", "focus")
+         .style("display", "none");
+
+      focus.append("circle")
+         .attr("r", 4.5);
+
+      focus.append("text")
+         .attr("x", 9)
+         .attr("dy", ".35em");
+
+      svg.append("rect")
+       .attr("class", "overlay")
+       .attr("width", width)
+       .attr("height", height)
+       .on("mouseover", function() { focus.style("display", null); })
+       .on("mouseout", function() { focus.style("display", "none"); })
+       .on("mousemove", mousemove);
+
+      function mousemove() {
+       var x0 = x.invert(d3.mouse(this)[0]),
+           i = bisectDate(data, x0, 1),
+           d0 = data[i - 1],
+           d1 = data[i],
+           d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+       focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
+       focus.select("text").text(d.close);
+     }
 
   });
 
